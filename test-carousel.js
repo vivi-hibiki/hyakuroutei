b234f156060ff2a5e522657162bc5c887fcf631b
vivi <ugomaru2012@yahoo.co.jp>
2023-04-06 23:52:09 +0900

    first commit

---
+++ test-carousel.js
@@ -0,0 +1,96 @@
+
+    
+    
+    $(function(){
+  $('.hamburger-menu').on('click', function() {
+    $(this).toggleClass('hamburger-menu-active')
+  })
+})
+  $(function(){
+    var effect_pos = 200; // 画面下からどの位置でフェードさせるか(px)
+    var effect_move = 50; // どのぐらい要素を動かすか(px)
+    var effect_time = 800; // エフェクトの時間(ms) 1秒なら1000
+
+    // フェードする前のcssを定義
+    $('.scroll-fade').css({
+        opacity: 0,
+        transform: 'translateY('+ effect_move +'px)',
+        transition: effect_time + 'ms'
+    });
+
+    // スクロールまたはロードするたびに実行
+    $(window).on('scroll load', function(){
+        var scroll_top = $(this).scrollTop();
+        var scroll_btm = scroll_top + $(this).height();
+        effect_pos = scroll_btm - effect_pos;
+
+        // effect_posがthis_posを超えたとき、エフェクトが発動
+        $('.scroll-fade').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( effect_pos > this_pos ) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+            }
+        });
+    });
+});
+$(function(){
+    var effect_pos = 100; // 画面下からどの位置でフェードさせるか(px)
+    var effect_move = 50; // どのぐらい要素を動かすか(px)
+    var effect_time = 800; // エフェクトの時間(ms) 1秒なら1000
+
+    // フェードする前のcssを定義
+    $('.scroll-fade2').css({
+        opacity: 0,
+        transform: 'translateY('+ effect_move +'px)',
+        transition: effect_time + 'ms'
+    });
+
+    // スクロールまたはロードするたびに実行
+    $(window).on('scroll load', function(){
+        var scroll_top = $(this).scrollTop();
+        var scroll_btm = scroll_top + $(this).height();
+        effect_pos = scroll_btm - effect_pos;
+
+        // effect_posがthis_posを超えたとき、エフェクトが発動
+        $('.scroll-fade2').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( effect_pos > this_pos ) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+            }
+        });
+    });
+});
+$(function() {
+	$('body').fadeIn(1000); //1秒かけてフェードイン！
+});
+jQuery(function() {
+  var appear = false;
+  var pagetop = $('#page_top');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+      if (appear == false) {
+        appear = true;
+        pagetop.stop().animate({
+          'bottom': '50px' //下から50pxの位置に
+        }, 300); //0.3秒かけて現れる
+      }
+    } else {
+      if (appear) {
+        appear = false;
+        pagetop.stop().animate({
+          'bottom': '-50px' //下から-50pxの位置に
+        }, 300); //0.3秒かけて隠れる
+      }
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+    return false;
+  });
+});
